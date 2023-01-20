993166a246f8a7ee664f78500f6e74516bc9a93d
atdushi <atdushi@example.com>
2023-01-21 02:37:36 +0300

    result datasets

--- clickhouse/result.sql
+++ clickhouse/result.sql
@@ -1,4 +1,12 @@
-select category , count(*) news_count
-from news n 
+with 1 as dataset
+select 
+	category,
+	count(*) "Всего новостей",
+	countIf(title, site='LENTA.RU') "LENTA.RU",
+	countIf(title, site='ВЕДОМОСТИ') "ВЕДОМОСТИ",
+	countIf(title, site='TASS') "TASS",
+	countIf(title, site='Фонтанка.ру') "Фонтанка.ру"
+from news n
+where (toDate(pub_date) = yesterday() and dataset=2) or dataset=1
 group by category
 order by category
